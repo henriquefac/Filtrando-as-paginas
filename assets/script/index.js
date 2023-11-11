0d748bfde95df0cbdc1c86e191658ee11c32cff3
Alexandre Franco <alexandrepinto586@gmail.com>
2023-11-11 18:36:25 -0300

    Retirada do iframe, correção ortográfica do README, inserção do tema escuro por js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,12 @@
 function changeTheme() {
-    var elemento_um = document.getElementsByClassName('navbar');
-    console.log(elemento_um);
+    var nav = document.getElementsByTagName('nav')
+    var body = document.getElementsByTagName('body')
+    var btn = document.getElementsByTagName('button')
+    console.log(btn);
+    console.log(body[0]);
+    if (nav[0].className == "navbar navbar-expand-md fixed-top navbar-light bg-light") {
+        nav[0].className += " navbar-dark bg-dark"
+    } else {
+        nav[0].className = "navbar navbar-expand-md fixed-top navbar-light bg-light"
+    }
 }
